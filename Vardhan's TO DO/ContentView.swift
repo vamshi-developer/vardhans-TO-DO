9960547581826a83ff96e07681364a3a30843eef
TONDA VAMSHI VARDHAN <vamshivardhan133@gmail.com>
2026-03-12 08:27:56 +0530

    Initial Commit

---
+++ Vardhan's TO DO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vardhan's TO DO
+//
+//  Created by TONDA VAMSHI VARDHAN on 12/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vardhan's TO DO/Vardhan_s_TO_DOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vardhan_s_TO_DOApp.swift
+//  Vardhan's TO DO
+//
+//  Created by TONDA VAMSHI VARDHAN on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Vardhan_s_TO_DOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
